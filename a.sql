3b5d95231630a1c95949f0a7b7a7fb06015f2cea
markethand <84249549+markethand@users.noreply.github.com>
2022-05-24 00:02:45 +0300

    long strings fix

--- a.sql
+++ a.sql
@@ -10,9 +10,9 @@ grant all privileges on database kanban to kuser;
 
 \c kanban
 
-CREATE TABLE sotr (
-    sotr_id serial  PRIMARY KEY,
-    sotr_fio varchar (50) NOT NULL
+CREATE TABLE employee (
+    employee_id serial  PRIMARY KEY,
+    employee_fullname varchar (50) NOT NULL
 );
 
 CREATE TABLE  card (
@@ -22,17 +22,18 @@ CREATE TABLE  card (
     card_column      integer check (card_column in (1,2,3)) NOT NULL,
 
     card_kanban_id   integer NOT NULL, 
-    FOREIGN KEY (card_worker_id) REFERENCES sotr(sotr_id)
+    FOREIGN KEY (card_worker_id) REFERENCES employee(employee_id)
 );
 
-INSERT INTO sotr VALUES (0, 'Undefined');
-INSERT INTO sotr (sotr_fio) VALUES ('Bob');
-INSERT INTO sotr (sotr_fio) VALUES ('Alice');
-INSERT INTO sotr (sotr_fio) VALUES ('John');
-INSERT INTO sotr (sotr_fio) VALUES ('Valera');
+INSERT INTO employee VALUES (0, 'Undefined');
+INSERT INTO employee (employee_fullname) VALUES ('Bob');
+INSERT INTO employee (employee_fullname) VALUES ('Alice');
+INSERT INTO employee (employee_fullname) VALUES ('John');
+INSERT INTO employee (employee_fullname) VALUES ('Valera');
 
 INSERT INTO card (card_title, card_worker_id, card_column, card_kanban_id) VALUES ('Test Card 1', 0, 1, 0);
 INSERT INTO card (card_title, card_worker_id, card_column, card_kanban_id) VALUES ('Test Card 2', 0, 1, 1);
 INSERT INTO card (card_title, card_worker_id, card_column, card_kanban_id) VALUES ('Test Card 3', 0, 2, 0);
 INSERT INTO card (card_title, card_worker_id, card_column, card_kanban_id) VALUES ('Test Card 4', 0, 2, 1);
 INSERT INTO card (card_title, card_worker_id, card_column, card_kanban_id) VALUES ('Test Card 5', 0, 3, 0);
+
